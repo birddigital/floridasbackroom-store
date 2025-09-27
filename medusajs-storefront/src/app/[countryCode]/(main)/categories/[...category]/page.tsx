088e93c24e12c079fc3d7a4dfef4feeb8ced8c83
birddigital <adrien@adrienbird.com>
2025-09-26 20:15:56 -0400

    Force dynamic rendering for categories

--- medusajs-storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ medusajs-storefront/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -13,29 +13,11 @@ type Props = {
   }
 }
 
+export const dynamic = 'force-dynamic'
+export const dynamicParams = true
+
 export async function generateStaticParams() {
-  const product_categories = await listCategories()
-
-  if (!product_categories) {
-    return []
-  }
-
-  const countryCodes = await listRegions().then((regions) =>
-    regions?.map((r) => r.countries.map((c) => c.iso_2)).flat()
-  )
-
-  const categoryHandles = product_categories.map((category) => category.handle)
-
-  const staticParams = countryCodes
-    ?.map((countryCode) =>
-      categoryHandles.map((handle) => ({
-        countryCode,
-        category: [handle],
-      }))
-    )
-    .flat()
-
-  return staticParams
+  return []
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
